2e3f6daa5fe165f77ce84020594fcbf8c6ea75e7
Lucas Broda <lulubrod@gmail.com>
2025-10-20 20:13:38 +0200

    feat(App.tsx): correct import for Calendrier

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
-import './component/tableau/calendrier'
+import Calendrier from './component/tableau/calendrier'
 
 function App() {
 
   return (
-    <>
-      <Calendrier dateDebut={new Date()} />
-    </>
+    <Calendrier dateDebut={new Date()} />
   )
 }
 
