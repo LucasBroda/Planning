c9ffa89137649d2ed78dc6c687505f9a4d078109
lucas_broda <lulubrod@gmail.com>
2025-10-24 08:27:08 +0100

    style(calendrier): style

--- my-app/src/component/tableau/calendrier.tsx
+++ my-app/src/component/tableau/calendrier.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import TableauHeader from './tableauHeader/tableauHeader';
 import TableauBody from './tableauBody/tableauBody';
 import Sidebar from '../sidebar/sidebar';
+import { useCalendar } from '../../contexts/CalendarContext';
 
-interface CalendrierProps {
-    dateDebut: Date;
-}
-
-const Calendrier: React.FC<CalendrierProps> = ({ dateDebut }) => {
+const Calendrier: React.FC = () => {
+    const { currentWeekStart } = useCalendar();
+    
     return (
         <div className="calendrier-container">
-            <div className="d-flex h-100" style={{ margin: 0, padding: 0 }}>
-                <div style={{ width: '15%', margin: 0, padding: 0 }}>
-                    <Sidebar />
-                </div>
-                <div style={{ width: '85%', margin: 0, padding: 0 }}>
-                    <TableauBody dateDebut={dateDebut} />
+            {/* Structure principale avec conteneurs comme demandé dans le README */}
+            <div className="calendar-wrapper">
+                {/* Conteneur global avec titre + semaine */}
+                <div className="calendar-main">
+                    <TableauHeader />
+                    
+                    {/* Conteneur avec sidebar + calendrier */}
+                    <div className="d-flex h-100" style={{ margin: 0, padding: 0 }}>
+                        <div style={{ width: '15%', margin: 0, padding: 0 }}>
+                            <Sidebar />
+                        </div>
+                        <div style={{ width: '85%', margin: 0, padding: 0 }}>
+                            <TableauBody dateDebut={currentWeekStart} />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
